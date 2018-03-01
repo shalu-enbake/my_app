bc4b29994d9a2683663682b3bbf3ac46899d0fec
Shalu Tyagi <shalu.enbake@gmail.com>
2018-03-01 16:20:25 +0530

    change in the production.yml file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.default_url_options = { host: 'https://ror-task.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default charset: "utf-8"
+  config.action_mailer.smtp_settings = {
+   :authentication => :login,
+   :address => 'smtp.gmail.com',
+   :port => 587,
+   :domain => "gmail.com",
+   :user_name => "shalu.enbake@gmail.com",
+   :password => "dogkutta1121",
+   :enable_starttls_auto => true
+ }
 end
